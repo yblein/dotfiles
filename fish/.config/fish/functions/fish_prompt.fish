2dcef5b1d5bd76b3489a718760e4dfe413490054
Yoann Blein <yoann.blein@free.fr>
2018-06-27 23:13:48 +0200

    fish: add git info in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,12 @@
+set -g __fish_git_prompt_showdirtystate
+set -g __fish_git_prompt_showcolorhints
+set -g __fish_git_prompt_showupstream
+set -g __fish_git_prompt_char_dirtystate '±'
+set -g __fish_git_prompt_char_cleanstate ''
+set -g __fish_git_prompt_char_upstream_ahead '↑'
+set -g __fish_git_prompt_char_upstream_behind '↓'
+set -g __fish_git_prompt_char_upstream_diverged '↕'
+
 function fish_prompt
-	echo -n -s (set_color --bold blue) (prompt_pwd) (set_color --bold yellow) " » " (set_color normal)
+	echo -n -s (set_color --bold blue) (prompt_pwd) (set_color white) (__fish_git_prompt) (set_color --bold yellow) " » " (set_color normal)
 end
